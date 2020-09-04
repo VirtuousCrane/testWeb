1bf18c734511ad037e3f6a24cfb8539a732757ab
Akagi <ray.daroonpat.2020@gmail.com>
2020-09-04 13:11:12 +0700

    Added basic html template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 # Define website
 app = Flask(__name__)
@@ -6,15 +6,25 @@ app = Flask(__name__)
 # Define Pages
 @app.route("/")
 def home():
-    return "<h1>Main Page</h1>"     # Returns inline html
+    return render_template("index.html")     # Returns inline html
 
+'''
 @app.route("/<name>") # Passes the string in the url to the function parameter
 def user(name):
-    return f"Hello {name}"
+    return f"Hello {name}!"
 
+
+# Normal Redirect
 @app.route("/admin")
 def admin():
     return redirect(url_for("home"))    # Redirects to home
+
+
+@app.route("/admin")
+# Redirect to page with parameter
+def admin():
+    return redirect(url_for("user", name="Ad!"))
+'''
 
 if __name__ == "__main__":
     app.run()
